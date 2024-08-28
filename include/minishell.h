2e1556f7f263c2ea8e83147623ac5df42295a6bf
pamatya <hydroweb.pranujamatya@gmail.com>
2024-08-28 02:44:34 +0200

    Added a struct to store input history.

--- include/minishell.h
+++ include/minishell.h
@@ -4,5 +4,15 @@
 # include "../lib/Libft/include/libft.h"
 # include <stdio.h>
 # include <stdlib.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+
+typedef struct s_inputs
+{
+    char            *input;
+    struct s_inputs *prev;
+    struct s_inputs *next;
+}   t_inputs;
+
 
 # endif 
