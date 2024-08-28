73e517b2fae80d15276d1dd48bc467458ebc9cc0
pamatya <hydroweb.pranujamatya@gmail.com>
2024-08-28 02:46:18 +0200

    Added some code to test readline in the main src.

--- src/main.c
+++ src/main.c
@@ -1,9 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pamatya <pamatya@student.42heilbronn.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/28 02:45:11 by pamatya           #+#    #+#             */
+/*   Updated: 2024/08/28 02:45:42 by pamatya          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 # include "minishell.h"
 
 int main(int ac, char **av)
 {
-	(void) av;
-	(void) ac;
-	ft_putstr_fd("Hello World!\n", 1);
-	return 0;
+    char        *input;
+    // t_inputs history;
+    
+    (void) av;
+    (void) ac;
+    ft_putstr_fd("Welcome to Minishell!\n", 1);
+    
+    while (!(ft_strncmp(input, "exit", 4)))
+    {
+        input = readline("Mini-Schale$");
+        if (!input)
+            continue;
+        add_history(input); 
+    }
+    
+    return 0;
 }
